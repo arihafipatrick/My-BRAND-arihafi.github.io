d454774e4f4f5b57e90532e60804458acec0c911
ARIAHFI Patrick <arihafipatrick@gmail.com>
2022-12-02 07:22:33 +0200

    Add files via upload

--- register.js
+++ register.js
@@ -1,14 +1,23 @@
 function store(){
-
 let username,password;
 username=document.getElementById("username").value;
 password=document.getElementById("password").value;
-
+var status=false;  
 let user_records=new Array();
 user_records=JSON.parse(localStorage.getItem("users"))?JSON.parse(localStorage.getItem("users")):[]
-if(user_records.some((v)=>{return v.username==username}))
+if(username =="")
 {
-  alert("duplicate data");
+document.getElementById("usernamevar").innerHTML=  
+"Please enter your username";   
+}else if (password.length<6) {
+  document.getElementById("passwordvar").innerHTML=  
+"Password must be at least 6 char long"; 
+status=false;
+}
+else if (user_records.some((v)=>{return v.username==username})){
+document.getElementById("taken").innerHTML=  
+"Username alredy taken😔";  
+status=false;  
 }
 else
 {
@@ -26,16 +35,34 @@ localStorage.setItem("users",JSON.stringify(user_records));
 
 //checking
 function check(){
-    let username,password;
+  let adminpass,adminuser;
+let username,password;
 username=document.getElementById("userName").value;
 password=document.getElementById("userPassword").value;
+adminuser="arihafi";
+adminpass="arihafipatrick";
 
 let user_records=new Array();
 user_records=JSON.parse(localStorage.getItem("users"))?JSON.parse(localStorage.getItem("users")):[]
-if(user_records.some((v)=>{return v.username==username && v.password==password}))
+if (username =="")
+{
+document.getElementById("usernamevar").innerHTML=  
+"Please enter your username";   
+}else if (password.length<6) {
+  document.getElementById("passwordvar").innerHTML=  
+"Password must be at least 6 char long"; 
+status=false;
+} else if(username == adminuser && password == adminpass) {
+  alert("Admin Login Successful!");
+  window.location.href="admin.html";
+  let current_user=user_records.filter((v)=>{return v.username==username && v.password==password})[0]
+ localStorage.setItem('username',current_user.username);
+ localStorage.setItem('password',current_user.password);
+}
+else if(user_records.some((v)=>{return v.username==username && v.password==password}))
 {
   alert("Login Successful!");
-   window.location.href="admin.html";
+   window.location.href="blog.html";
   let current_user=user_records.filter((v)=>{return v.username==username && v.password==password})[0]
  localStorage.setItem('username',current_user.username);
  localStorage.setItem('password',current_user.password);
@@ -43,7 +70,11 @@ if(user_records.some((v)=>{return v.username==username && v.password==password})
 }
 else
 {
-  alert('Login Failed');
+  document.getElementById("invalid").innerHTML=  
+"Invalid Username or Password...😔";  
+status=false;  
 }
 
 }
+
+
